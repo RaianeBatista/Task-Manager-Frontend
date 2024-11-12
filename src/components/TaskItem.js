61e452fa795760e9c225ab5eaa5c9dd641433fc9
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-11-12 18:35:11 -0300

    refactor: access lifecycle methods in Function Component

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,33 +1,45 @@
-// const TaskItem = ({ task }) => {
-//   return (
-//     <>
-//       <h1>{task.description}</h1>
-//       <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
-//     </>
-//   );
-// };
+import { useEffect, useState } from 'react';
 
-// export default TaskItem;
+const TaskItem = ({ task }) => {
+  const [testState] = useState();
 
-import React from 'react';
-class TaskItem extends React.Component {
-  componentDidMount() {
-    console.log('component was mounted!');
-  }
+  useEffect(() => {
+      console.log('component was mounted!');
+      
+      return () => {
+          console.log('i will unmount!')
+      }
+  }, []);
 
-  componentWillUnmount() {
-    console.log('I will unmount!');
-  }
-
-  render() {
-    const { task } = this.props;
-    return (
-      <>
-        <h1>{task.description}</h1>
-        <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>{task.description}</h1>
+      <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
+    </>
+  );
+};
 
 export default TaskItem;
+
+// import React from 'react';
+// class TaskItem extends React.Component {
+//   componentDidMount() {
+//     console.log('component was mounted!');
+//   }
+
+//   componentWillUnmount() {
+//     console.log('I will unmount!');
+//   }
+
+//   render() {
+//     const { task } = this.props;
+//     return (
+//       <>
+//         <h1>{task.description}</h1>
+//         <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
+//       </>
+//     );
+//   }
+// }
+
+// export default TaskItem;
