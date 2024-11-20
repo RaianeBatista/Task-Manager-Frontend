ef802acc2dc1e86fcf2c4fa82c0a8ba7e3d345c2
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-11-20 16:44:09 -0300

    feat: fetch tasks from back-end, clean useless files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { useState, useRef, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import TaskItem from './components/TaskItem';
 
-//App é component pai de TasItem
 const App = () => {
-  const mounted = useRef(false);
-
-  useEffect(() => {
-    if (mounted.current === false) {
-      mounted.current = true;
-    } else {
-      console.log('component was updated!');
-    }
-  });
-
   const [tasks, setTasks] = useState([
     {
       id: '1',
@@ -26,68 +17,27 @@ const App = () => {
     },
   ]);
 
-  const handleCleanTasks = () => {
-  setTasks([])
-}
+  const fetchTasks = async () => {
+    try {
+      const {data} = await axios.get('http://localhost:8000/tasks');
+    
+      setTasks(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   return (
     <>
       {tasks.map((task) => (
         <TaskItem key={task.id} task={task} />
       ))}
-      <button onClick={handleCleanTasks}>Limpar tarefa</button>
     </>
   );
 };
 
 export default App;
-
-
-// Componentes de Classe
-// import React from 'react';
-// import TaskItem from './components/TaskItem';
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleStateChange = this.handleStateChange.bind(this);
-//     this.state = {
-//       tasks: [
-//         {
-//           id: '1',
-//           description: 'Estudar Programação',
-//           isCompleted: false,
-//         },
-//         {
-//           id: '2',
-//           description: 'Ler',
-//           isCompleted: true,
-//         },
-//       ],
-//     };
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log(prevState)
-//   console.log('component was updated!')
-// }
-
-//   handleStateChange() {
-//     this.setState({
-//       tasks: [],
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         {this.state.tasks.map((task) => (
-//           <TaskItem key={task.id} task={task} />
-//         ))}
-//         <button onClick={this.handleStateChange}>Limpar tarefas</button>
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,14 +1,4 @@
-import { useEffect, useState } from 'react';
-
 const TaskItem = ({ task }) => {
-  useEffect(() => {
-      console.log('component was mounted!');
-      
-      return () => {
-         console.log('i will unmount!')
-      }
-  }, []);
-
   return (
     <>
       <h1>{task.description}</h1>
@@ -18,27 +8,3 @@ const TaskItem = ({ task }) => {
 };
 
 export default TaskItem;
-
-// Componentes de Classe
-// import React from 'react';
-// class TaskItem extends React.Component {
-//   componentDidMount() {
-//     console.log('component was mounted!');
-//   }
-
-//   componentWillUnmount() {
-//     console.log('I will unmount!');
-//   }
-
-//   render() {
-//     const { task } = this.props;
-//     return (
-//       <>
-//         <h1>{task.description}</h1>
-//         <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
-//       </>
-//     );
-//   }
-// }
-
-// export default TaskItem;
