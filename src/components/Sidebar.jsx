b1cf02952c8374396553061e8dec48ba33a98cb3
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-11-21 18:23:17 -0300

    feat: add Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,20 @@
 import './Sidebar.scss';
 
-import CustomButton from './CustomButton'
-import logo from '../assets/images/logo.png'
-
+import CustomButton from './CustomButton';
+import logo from '../assets/images/logo.png';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar-container">
-            <div className="logo">
-                <img src={logo} alt="Full Satck Club" />
-            </div>
+  return (
+    <div className='sidebar-container'>
+      <div className='logo'>
+        <img src={logo} alt='Full Satck Club' />
+      </div>
 
-            <div className="sign-out">
-                <CustomButton>
-                    Sair
-                </CustomButton>
-            </div>
-</div>
-    )
-}
+      <div className='sign-out'>
+        <CustomButton>Sair</CustomButton>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
